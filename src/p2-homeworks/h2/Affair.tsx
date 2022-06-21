14bd0a2c3f0a1471dca901a078e0920de66a6781
AlesanderZabrodski09 <zabrodski666@gmail.com>
2022-06-21 19:54:08 +0200

    fix: Affair.tsx

--- src/p2-homeworks/h2/Affair.tsx
+++ src/p2-homeworks/h2/Affair.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import {AffairType} from './HW2';
 
 type AffairPropsType = {
   // key не нужно типизировать
-  affair: any // need to fix any
-  deleteAffairCallback: () => void // need to fix any
+  affair: AffairType // need to fix any
+  deleteAffairCallback: (id: number) => void // need to fix any
 }
 
 function Affair(props: AffairPropsType) {
   const deleteCallback = () => {
-    props.deleteAffairCallback
+    props.deleteAffairCallback(props.affair._id)
   }// need to fix
 
   return (
