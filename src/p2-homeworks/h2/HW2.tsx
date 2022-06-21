cef715b66d608f14caf20833e9080e837b5b6aff
AlesanderZabrodski09 <zabrodski666@gmail.com>
2022-06-21 19:53:38 +0200

    fix: HW2

--- src/p2-homeworks/h2/HW2.tsx
+++ src/p2-homeworks/h2/HW2.tsx
@@ -22,13 +22,22 @@ const defaultAffairs: Array<AffairType> = [ // need to fix any
 // pure helper functions
 export const filterAffairs = (affairs: Array<AffairType>, filter: FilterType): Array<AffairType> => { // need to fix any
   if (filter === 'all') return affairs
-  else if (filter === 'low') return affairs.filter((data) => date.priority === 'low')// need to fix
+    // else if (filter === 'low') return affairs.filter((a) => (a.priority === 'low'));
+    // else if (filter === 'middle') return affairs.filter(a => a.priority === 'middle');
+    // else if (filter === 'high') return affairs.filter((a) => (a.priority === 'high'));
+    // else {
+    // }
+  // return []
+  else return affairs.filter(a => a.priority === filter)
+  // need to fix
 }
-export const deleteAffair = (affairs: any, _id: number): any => { // need to fix any
-  return // need to fix
+export const deleteAffair = (affairs: Array<AffairType>, _id: number): Array<AffairType> => { // need to fix any
+  return affairs.filter(a => a._id !== _id)// need to fix
 }
 
 function HW2() {
+
+
   const [affairs, setAffairs] = useState<Array<AffairType>>(defaultAffairs) // need to fix any
   const [filter, setFilter] = useState<FilterType>('all')
 
